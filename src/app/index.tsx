fa543a8504160b21f6b537a64012f1fc8467282f
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-07-19 20:02:09 +0700

    feat: modify structure folder, improve structure

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,8 +1,21 @@
-import MyRoutes from '@/app/routes'
+import LayoutMain from '@/layouts'
+
+import { Suspense, lazy } from 'react'
+import { Route, Routes } from 'react-router-dom'
+
+const HomePage = lazy(() => import('@/pages').then((m) => ({ default: m.HomePage })))
 
 // you can add Header, footer anything else you might want to, or else leave it to be
 const App = () => {
-  return <MyRoutes />
+  return (
+    <Suspense fallback={null}>
+      <Routes>
+        <Route path='/' element={<LayoutMain />}>
+          <Route index element={<HomePage />} />
+        </Route>
+      </Routes>
+    </Suspense>
+  )
 }
 
 export default App
---
+++ src/pages/home.tsx
@@ -0,0 +1,9 @@
+const Home: React.FunctionComponent = () => {
+  return (
+    <section className='py-10 md:py-10'>
+      <h1>Stravital</h1>
+    </section>
+  )
+}
+
+export default Home
