7d4d7e4f444f2a260737e150067060973ed46226
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-08-12 01:29:05 +0700

    feat: add 404 page

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,12 +2,13 @@ import { useTheme } from '@/hooks'
 import LayoutMain from '@/layouts'
 
 import { Suspense, lazy } from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
 
 const HomePage = lazy(() => import('@/pages').then((m) => ({ default: m.HomePage })))
+const NotFoundPage = lazy(() => import('@/pages').then((m) => ({ default: m.NotFoundPage })))
 
 // you can add Header, footer anything else you might want to, or else leave it to be
-const StravitalApp = () => {
+const StravitalApp: React.FunctionComponent = () => {
   // useTheme shold always on top of app
   useTheme()
   return (
@@ -16,6 +17,8 @@ const StravitalApp = () => {
         <Route path='/' element={<LayoutMain />}>
           <Route index element={<HomePage />} />
         </Route>
+        <Route path='/404' element={<NotFoundPage />} />
+        <Route path='*' element={<Navigate to='/404' />} />
       </Routes>
     </Suspense>
   )
